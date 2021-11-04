ba41f7ce33e142b2983fcd24c166b67074a899a4
Eguzki <eguzkia@lyft.com>
2021-11-04 16:47:33 -0600

    bugfixing

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,4 @@
 class Submission < ApplicationRecord
+    has_many :answers
     belongs_to :quiz
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_222222) do
+ActiveRecord::Schema.define(version: 2021_11_04_222319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_222222) do
 
   create_table "submissions", force: :cascade do |t|
     t.string "name"
+    t.integer "quiz_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
